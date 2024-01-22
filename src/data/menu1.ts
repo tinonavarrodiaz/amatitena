652cce67bda25d1b72eed41daf6e27057d8b8165
Florenino Navarro Diaz <tino.navarro@hotmail.com>
2024-01-22 14:39:35 -0600

    done

---
+++ src/data/menu1.ts
@@ -0,0 +1,22 @@
+export const menu1 = [
+  {
+    title: 'About Us',
+    link: '../#about-us',
+  },
+  {
+    title: 'Tequilas',
+    link: '../#tequilas',
+  },
+  {
+    title: 'Our lands',
+    link: '/our-lands',
+  },
+  {
+    title: 'Where to Buy',
+    link: '../#where-to-buy',
+  },
+  {
+    title: 'Contact',
+    link: '../#contact',
+  },
+];
